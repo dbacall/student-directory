131be79d49cbe1dc203b3261ee27bd10110b1d10
dbacall <dbacall@hotmail.co.uk>
2019-10-25 13:55:20 +0000

    Refactored code

--- directory.rb
+++ directory.rb
@@ -1,24 +1,26 @@
+@students = []  
+
 def input_students
   puts "Please enter the names of the students"
   puts "Which cohort are you in?"
   puts "Please enter their hobby"
   puts "To finish, just hit return three times"
   
-  students = []
+  
   
   name = gets.chomp
   cohort = gets.chomp.to_sym
   hobby = gets.chomp
   while !name.empty? do
       if cohort.empty?
-    students << {name: name, cohort: :unknown, hobby: hobby}
+        @students << {name: name, cohort: :unknown, hobby: hobby}
       else
-        students << {name: name, cohort: cohort, hobby: hobby}
+        @students << {name: name, cohort: cohort, hobby: hobby}
       end
-    if students.length > 1
-    puts "Now we have #{students.count} students"
+    if @students.length > 1
+    puts "Now we have #{@students.count} students"
     else
-    puts "Now we have #{students.count} student"   
+    puts "Now we have #{@students.count} student"   
     end
     puts "Add another name"
     puts "Which cohort?"
@@ -27,7 +29,7 @@ def input_students
     cohort = gets.chomp.to_sym
     hobby = gets.chomp
   end
-  students
+  @students
 end
 
 
@@ -55,30 +57,38 @@ def print_footer(names)
   end
 end
 
-def interactive_menu
-  students = []    
-  loop do
-     puts "1. Input the students"
-     puts "2. Show the students"
-     puts "9 Exit"
-     
-     selection = gets.chomp
-     
-     case selection
-       when "1"
-         students = input_students
-       when "2"
-         while students.length > 0 do
-           print(students)
-           print_footer(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  while @students.length > 0 do
+           print_students_list(@students)
+           print_footer(@students)
            break
-         end
-       when "9"
-         exit
-       else
-         puts "I don't know what you meant, try again"
-     end
-      
+  end
+end
+
+
+def process(selection)
+  case selection
+    when "1"
+      @students = input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+     print_menu
+     process(gets.chomp)
   end
 end
 
